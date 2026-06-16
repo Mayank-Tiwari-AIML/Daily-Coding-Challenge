d9904078d83a3075376e31655c1119827b7c5d2d
Mayank Kailas Tiwari <mayanktiwarimaurya@gmail.com>
2026-06-16 21:02:06 +0530

    Day 5 Completed

--- Day 4/92.cpp
+++ Day 4/92.cpp
@@ -1,13 +1,14 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
+#include <bits/stdc++.h>
+
+//  Definition for singly-linked list.
+  struct ListNode {
+      int val;
+      ListNode *next;
+      ListNode() : val(0), next(nullptr) {}
+      ListNode(int x) : val(x), next(nullptr) {}
+     ListNode(int x, ListNode *next) : val(x), next(next) {}
+  };
+
 class Solution {
 public:
     pair<ListNode*, ListNode*> reverse(ListNode* &start, ListNode* &end){
