94e093dfac2bcc1a539b43bb9f14c6a9a8576df3
Mayank Kailas Tiwari <mayanktiwarimaurya@gmail.com>
2026-06-18 21:04:32 +0530

    Completed Day 7

---
+++ Day 7/287.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int slow=nums[0];
+        int fast=nums[0];
+        while(true){
+            slow=nums[slow];
+            fast=nums[nums[fast]];
+            if(slow==fast) break;
+        }
+        fast=nums[0];
+        while(slow!=fast){
+            slow=nums[slow];
+            fast=nums[fast];
+        }
+        return slow;
+    }
+};
