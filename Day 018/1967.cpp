98c11cd61e9705033a13b6e7c73b2199513589d4
Mayank Kailas Tiwari <mayanktiwarimaurya@gmail.com>
2026-06-29 21:38:38 +0530

    Completed Day 18

---
+++ Day 018/1967.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int numOfStrings(vector<string>& patterns, string word) {
+        int ans = 0;
+
+        for (string &s : patterns) {
+            if (word.find(s) != string::npos)
+                ans++;
+        }
+
+        return ans;
+    }
+};
